9b112a09a94acbf673149633c3ed8a4725fb6522
nobuyuki_nyuu <nobuyuki.nyuu@gmail.com>
2021-07-11 23:46:17 -0500

    Version 2.0 (for Godot 3.3.2) -Zoom level now is a dropdown for typical presets -Mouse position significant digits changes depending on zoom level

--- addons/zoomy/zoomy.gd
+++ addons/zoomy/zoomy.gd
@@ -9,18 +9,25 @@ var c # A class member to hold the widget control during the plugin lifecycle
 func _enter_tree():
 	print("Activating zoomy..")
 	# Initialization of the plugin goes here
-	c = preload("res://addons/zoomy/Zoom Level.tscn").instance()
+	self.c = load("res://addons/zoomy/Zoom Level.tscn").instance()
 
 	add_control_to_container(CONTAINER_CANVAS_EDITOR_BOTTOM, c)
 	
-	c.editorb = self.get_editor_interface()
+#	c.editorb = self.get_editor_interface()
 	c.activate()
-	connect("main_screen_changed",c,"refresh")
-	connect("scene_changed",c,"refresh")
+
+	#Godot 3 specific
+	connect("main_screen_changed",c, "refresh")
+	connect("scene_changed",c, "refresh")
+
+#	#Godot 4 specific
+#	connect("main_screen_changed", Callable(c, "refresh"))
+#	connect("scene_changed", Callable(c, "refresh"))
+
 
 func _exit_tree():
 	# Clean-up of the plugin goes here
 	print("Deactivating zoomy..")
 	remove_control_from_container(CONTAINER_CANVAS_EDITOR_BOTTOM, c) # Remove the dock
-	c.free() # Erase the control from the memory
+	c.queue_free() # Erase the control from the memory
 	
